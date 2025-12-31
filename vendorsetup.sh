75597dfcac4e88b11a87f48525585d83ce734b68
BllsszzXD <filecloudrom@gmail.com>
2025-12-31 18:04:05 +0700

    X00TD : Regenerate

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-
-export TZ=Asia/Jakarta
-
-rm -rf kernel/asus/sdm660
-git clone --single-branch --recursive https://github.com/strongreasons/android_kernel_asus_sdm660 -b clo kernel/asus/sdm660
-#cd kernel/asus/sdm660
-#git revert 079a547168e74a57b7431652554eddef0e901a59 --no-edit
-#cd ../../..
-
-# Force Using KSU
-rm -rf system/extras/su
-
 rm -rf vendor/lineage-priv/keys
 mkdir -p vendor/lineage-priv/keys
 git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
